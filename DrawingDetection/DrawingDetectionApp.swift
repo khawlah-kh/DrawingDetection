15e83ea2cf21e8ec860818ca0cfa4a09865384f2
Khawlah Khalid <khawlah1920@hotmail.com>
2023-05-22 12:26:20 +0300

    Initial Commit

---
+++ DrawingDetection/DrawingDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingDetectionApp.swift
+//  DrawingDetection
+//
+//  Created by Khawlah Khalid on 22/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
